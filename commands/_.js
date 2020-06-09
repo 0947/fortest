aa153d0a088ea6c0c9f7a53d168bf69b0903df17
Bots.Business <git.sync@bots.business>
2020-06-09 18:46:21 +0000

    Bots.Business export for bot id: 87893

--- commands/_.js
+++ commands/_.js
@@ -12,11 +12,10 @@
   aliases: 
 CMD*/
 
-
 Bot.sendInlineKeyboard(
   [
   { title: "↪️ወደ ዋና ገፅ↩️", command: "/start"}], 
-"ውድ  "+ user.first_name+"   እባኮትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገፅ ለመሄድ ከታች ያለውን በተን ይጫኑት፡፡",{on_result:"/msgid"})
+"ውድ  "+ user.first_name+"   እባኮትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገፅ ለመሄድ ከታች ያለውን በተን ይጫኑት፡፡")
 let admin_text ="<b>S.From:</b> \n" +'<a href="' +"tg://user?id=" + user.telegramid +'">' + user.first_name + "</a>" +" \nText: <code>" + message + " </code>"
 let ADMIN = "@xexgbjnsjnjudxbjjdxSam"
   Api.sendMessage({ chat_id: ADMIN, text: admin_text, parse_mode: "html"})
