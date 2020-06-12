49f9c6b4b147c779383c0fb6376f5c83fddc6ce3
Bots.Business <git.sync@bots.business>
2020-06-12 13:43:38 +0000

    Bots.Business export for bot id: 87893

--- commands/_gexpdf.js
+++ commands/_gexpdf.js
@@ -16,6 +16,6 @@ var b = [[{ text: "ወደ ዋና ገጽ", callback_data: "/mych"}]]
 
  Api.sendDocument({
   chat_id: chat.chatid,
-  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/626",caption:  "ልዩ የሐዋርያዊ ተልእኮ ዘመን\n\n💦የተጠመቁና የተላኩ💦\n\nየክርስቶስ ቤተክርስቲያን ተልኮ በአለም\n✅2012 ዓ.ም ግፃዌ✅\n\n💧Baptised and sent💧\n\nthe church of chirst on mission in the world\n〰extraordinary mission month - October 2019", reply_markup: { inline_keyboard: b } })
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/626",caption:  "ልዩ የሐዋርያዊ ተልእኮ ዘመን\n\n💦የተጠመቁና የተላኩ💦\n\nየክርስቶስ ቤተክርስቲያን ተልኮ በአለም\n✅2012 ዓ.ም ግፃዌ✅\n\n💧Baptised and sent💧\n\nthe church of chirst on mission in the world\n〰extraordinary mission month - October 2019", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
