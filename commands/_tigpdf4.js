ecdea3e335139e2c43c7ca89da705033d367667b
Bots.Business <git.sync@bots.business>
2020-06-12 16:40:32 +0000

    Bots.Business export for bot id: 87893

--- commands/_tigpdf4.js
+++ commands/_tigpdf4.js
@@ -9,17 +9,19 @@
   aliases: 
 CMD*/
 
+let msg=User.getProperty("msgid")
+
 Api.sendDocument({
   chat_id: chat.chatid,
   document: "https://t.me/xexgbjnsjnjudxbjjdxSam/86"})
 Api.sendDocument({
   chat_id: chat.chatid,
   document: "https://t.me/xexgbjnsjnjudxbjjdxSam/87"})
+
+var b = [[{ text: "ተመለስ", callback_data: "/mych"}]]
+
 Api.sendDocument({
   chat_id: chat.chatid,
-  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/88"})
-var b =[
-[{ title: "✨Page1⃣", command: "/tigpdf1" },{ title: "✨Page2⃣", command: "/tigpdf2" }],
-[{ title: "✨Page3⃣", command: "/tigpdf3" },{ title: "✨Page4⃣", command: "/tigpdf4" }],
-[{ title: "⬆️ወደ ዋናገፅ ይመለሱ⬆️ ", command: "/mych" }]]
-Bot.sendInlineKeyboard(b,"👇🏽ከታች የሚፈልጉትን ገፅ ቁጥር ይምረጡ")
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/88)",reply_markup: { inline_keyboard: b } })
+
+Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
