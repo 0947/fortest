9c6ef51ca0a8a0fabd95b921a712db7aa14f8a20
Bots.Business <git.sync@bots.business>
2020-06-12 14:37:49 +0000

    Bots.Business export for bot id: 87893

--- commands/_covid.js
+++ commands/_covid.js
@@ -15,18 +15,18 @@ var b = [[{ text: "⬆️ዋና ገፅ", callback_data: "/mych"}]]
 
  Api.sendVideo({
   chat_id: chat.chatid,
-video:"https://t.me/xexgbjnsjnjudxbjjdxSam/32",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b } })
+video:"https://t.me/xexgbjnsjnjudxbjjdxSam/32",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.sendVideo({
   chat_id: chat.chatid,
-video:"https://t.me/xexgbjnsjnjudxbjjdxSam/33",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b } })
+video:"https://t.me/xexgbjnsjnjudxbjjdxSam/33",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.sendVideo({
   chat_id: chat.chatid,
-video:"https://t.me/xexgbjnsjnjudxbjjdxSam/34",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b } })
+video:"https://t.me/xexgbjnsjnjudxbjjdxSam/34",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.sendVideo({
   chat_id: chat.chatid,
-video:"https://t.me/xexgbjnsjnjudxbjjdxSam/35",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b } })
+video:"https://t.me/xexgbjnsjnjudxbjjdxSam/35",caption:  "📛ሰሞንኛ ፕሮግራም ከኮቪድ ወረርሽኝ ጋር የተያያዘ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
