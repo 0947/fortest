7f9377c8c643c2e5718c1725c82f2e912d6831d2
Bots.Business <git.sync@bots.business>
2020-06-16 21:56:26 +0000

    Bots.Business export for bot id: 87893

--- commands/_bt41.js
+++ commands/_bt41.js
@@ -12,6 +12,6 @@ CMD*/
 let msg = User.getProperty("msgid");
 
 var b =[
-[{title:"⏩ለመመለስ⏪",command:"/tigmez"},{title:"🔼ዋና ገፅ🔽",command:"/mych"}]]
+[{title:"⏩ለመመለስ⏪",command:"/tigmez"},{title:"🔼ዋና ገፅ🔽",command:"/mytig"}]]
 
 Bot.editMessage("🌷   ተስፋ ኣይቆርፅን ኢየ  🌷\n1,ሓጥያተይ ከቢዱኒ ዒቢ ፅዕነት ኮይኑኒ\nማርያም ኣማልድለይ ወድኺ ክምሕረኒ \n\nተደ:ተስፋ ኣይቆርፅን'የ /2/\n   ክሳብ ተማልድለይ ኣነ ኣብ ትሕቴኺ'የ\n\n2,ሰይጣን እንተፈተነኒ ኢሉ ከውድቐኒ\nርሕርሕቲ ወላዲተይ ንስኺ ኣለኽኒ\n\n3,ብሓጥያተይ ዝኣክል ሕልናይ ከይወቕሰኒ\nንድሕሪት ከይምለስ ኣደይ ሓግዝኒ\n\n4,ሕልናይ ተሸቢሩ ሰላም'ውን ስኢነ\nእልምን ኣለኹ ኣብ እግርኺ ኮይነ\n\nተደ: ተስፋ ኣይቖርፅን'የ \n\n             🌷🙏🙏🙏🌷\n\nብመዘምራን ደብረ ቅ.ኣንጠንዮስ ጎዳይፍ ኣስመራ vol#1",msg)&Bot.editInlineKeyboard(b,msg)
---
+++ commands/_edokid.js
@@ -0,0 +1,48 @@
+/*CMD
+  command: /edokid
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let msg=User.getProperty("msgid")
+
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2227"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2228"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2229"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2230"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2231"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2232"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2233"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2234"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2235"})
+
+var b = [[{ text: "ተመለስ", callback_data: "/mywl"}]]
+
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2236)",reply_markup: { inline_keyboard: b } })
+
+Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
