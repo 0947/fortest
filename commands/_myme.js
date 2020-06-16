6329e2db2accebc074c23dad8da4df152a38bdfc
Bots.Business <git.sync@bots.business>
2020-06-16 10:02:55 +0000

    Bots.Business export for bot id: 87893

--- commands/_myme.js
+++ commands/_myme.js
@@ -13,17 +13,17 @@ let msg=User.getProperty("msgid")
 
 var b =[
 [{ title: "🏠የካቶሊክ ቤተክርስቲያን ምንነት", command: "/first" }],
-[{ title:"🇪🇹የትግርኛ ገፅ🇪🇹", command:"/tigray"}],
 [ { title: "💒ስብከቶች", command: "/thalk" },{title:"⚠️COVID ፕሮግራሞች",command:"/covid"}],
 [{title:"📜የቅዱሳን ታሪክ📜",command:"/kidus"},{title:"🙀የቮካል ስልጠና",command:"/vocal"}],
-[{title:"🗞ግፃዌ",command:"/gex"},{title:"🔅ግዕዝ ዜማ",command:"/geez"}],
+[{title:"🗞ግፃዌ",command:"/gex"},{title:"🔅ግዕዝ ዜማ",command:"/geezs"}],
 [{title:"✝ጸሎት",command:"/xelo"},{title:"🎬ሲኒማ",command:"/catcin"}],
 [{title:"📚PDF መፅሀፍት",command:"/cpdf"},{title:"📡የቫቲካን ራዲዮ",command:"/cvati"}],
 [{title:"🛡Application",command:"/capps"},{title:"🕎የቅዳሴ ስርአት",command:"/kidse"}],
 [{title:"🎬ቪዲዮ",command:"/cvids"},{title:"✴️የቅዱሳን አባባል",command:"/abkidz"}],
 [{title:"🎹keyboard",command:"/kibstl"},{title:"🇻🇦ፓፓ ፍራንሲስ",command:"/popfran"}],
 [{title:"😇ፆታዊ ቅድስና",command:"/theolo"},{title:"🎧መዝሙሮች",command:"/mzmu"}],
-[{ title: "👤Comment", command: "/admin" },{title:"⁉️FAQ",command:"/faqsami"}]]
+[{ title: "👤Comment", command: "/admin" },{title:"⁉️FAQ",command:"/faqsami"}],
+[{title:"🌍ቋንቋ ቀይር / Change Language🌐",command:"/mytg"}]]
 
 var th="["+user.first_name+"]("+"tg://user?id="+user.telegramid+")"
 
