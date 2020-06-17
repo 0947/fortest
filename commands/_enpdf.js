ff668250fa81cb3ae97fe998502c335d299a6e1e
Bots.Business <git.sync@bots.business>
2020-06-17 06:06:28 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_enpdf.js
@@ -0,0 +1,20 @@
+/*CMD
+  command: /enpdf
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let msg=User.getProperty("msgid")
+
+var b = [[{ text: "Go Back", callback_data: "/encat"}]]
+
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2364)",reply_markup: { inline_keyboard: b },on_result: "/msgid"})
+
+Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
