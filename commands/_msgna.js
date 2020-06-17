93674869228f13f57baa0ef65519679a3d085a90
Bots.Business <git.sync@bots.business>
2020-06-17 16:27:17 +0000

    Bots.Business export for bot id: 87893

--- commands/_msgna.js
+++ commands/_msgna.js
@@ -41,5 +41,5 @@ Api.sendDocument({
   document: "https://t.me/xexgbjnsjnjudxbjjdxSam/121"})
 
 var b=[
-[{title:" ↕️ዋና ገፅ ", command :"/mych"}]]
+[{title:" ↕️ዋና ገፅ ", command :"/mytig"}]]
 Bot.sendInlineKeyboard(b,"ወደ ኃላ ወይንም ወደ ዋና ማውጫ ይመለሱ")
---
+++ commands/_samtigray.js
@@ -0,0 +1,28 @@
+/*CMD
+  command: /samtigray
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let user = options.result.status
+
+if (user == "member" || user == "creator") {
+  Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text:"🛡Loading Now...",
+  show_alert: " top" })
+  
+Bot.runCommand("/tigray")
+} else {
+  Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text:"⚠️ቻነሉን አልተቀላቀሉም በድጋሚ ይሞክሩ‼️",
+  show_alert: "top"
+})}
+
+Bot.setProperty("status", user, "text")
