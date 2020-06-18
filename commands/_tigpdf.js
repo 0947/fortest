1fbe2f87e0ae3f80bed385355b5bae548c59fc72
Bots.Business <git.sync@bots.business>
2020-06-18 20:18:09 +0000

    Bots.Business export for bot id: 87893

--- commands/_tigpdf.js
+++ commands/_tigpdf.js
@@ -14,6 +14,6 @@ let msg = User.getProperty("msgid");
 var b =[
 [{ title: "✨Page1⃣", command: "/tigpdf1" },{ title: "✨Page2⃣", command: "/tigpdf2" }],
 [{ title: "✨Page3⃣", command: "/tigpdf3" },{ title: "✨Page4⃣", command: "/tigpdf4" }],
-[{ title: "⬆️ወደ ዋናገፅ ይመለሱ⬆️ ", command: "/mych" }]]
+[{ title: "⬆️ወደ ዋናገፅ ይመለሱ⬆️ ", command: "/mytig" }]]
 
 Bot.editMessage("ከታች የሚፈልጉትን የገፅ ቁጥር ይምረጡ፡፡",msg)&Bot.editInlineKeyboard(b,msg)
