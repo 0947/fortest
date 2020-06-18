ebd4249d51ead5929fe12d0ab5049d42a7b9a98e
Bots.Business <git.sync@bots.business>
2020-06-18 11:16:23 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_dskl.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /dskl
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+Bot.setProperty("ghg" , data.message,  "string");
+
+Bot.sendMessage("message set")
