cf834125280f892318e114db6bab42464b9e6bc6
Bots.Business <git.sync@bots.business>
2020-06-19 03:58:06 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_sgju.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /sgju
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+Bot.setProperty("lfc" , data.message,  "string");
+
+Bot.sendMessage("message set")
