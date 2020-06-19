57c1b4df62e1fdf30bcb2b9fe39657d0298cc4f1
Bots.Business <git.sync@bots.business>
2020-06-19 21:42:24 +0000

    Bots.Business export for bot id: 87893

--- commands/_tigmek.js
+++ commands/_tigmek.js
@@ -20,5 +20,5 @@ Api.sendDocument({
   document: "https://t.me/xexgbjnsjnjudxbjjdxSam/403"})
 
 var b=[
-[{title:" ↕️ዋና ገፅ ", command :"/mych"}]]
+[{title:" ↕️ዋና ገፅ ", command :"/mytig"}]]
 Bot.sendInlineKeyboard(b,"ወደ ኃላ ወይንም ወደ ዋና ማውጫ ይመለሱ")
---
+++ commands/_zarpol.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /zarpol
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+Bot.setProperty("xxc" , data.message,  "string");
+
+Bot.sendMessage("message set")
