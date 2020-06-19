51b20cf3095e15662abc4d00a0f796635305c7e4
Bots.Business <git.sync@bots.business>
2020-06-19 22:13:19 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_kljh.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /kljh
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+Bot.setProperty("cbv" , data.message,  "string");
+
+Bot.sendMessage("message set")
---
+++ commands/_replaymulen.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /replaymulen
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+
+Bot.runCommand("/samxen");
+User.setProperty("replyid", params, "integer");
