bb61b82de051da7a0fa174908e322fc7779ed1ba
Bots.Business <git.sync@bots.business>
2020-06-19 06:12:59 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_gur.js
@@ -0,0 +1,28 @@
+/*CMD
+  command: /gur
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let user = options.result.status
+
+if (user == "member" || user == "creator") {
+  Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text:"✅Wait downloading from server.....",
+  show_alert: " top" })
+  
+Bot.runCommand("/gusam")
+} else {
+  Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text:"⚠️Join the channel first‼️",
+  show_alert: "top"
+})}
+
+Bot.setProperty("status", user, "text")
