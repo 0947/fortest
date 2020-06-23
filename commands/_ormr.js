2f191b751f9ea32fa8a32fa5abb8c75734469fa2
Bots.Business <git.sync@bots.business>
2020-06-23 18:51:09 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/_ormr.js
@@ -0,0 +1,42 @@
+/*CMD
+  command: /ormr
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let msg=User.getProperty("msgid")
+
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2309"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2310"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2311"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2312"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2313"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2314"})
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2315"})
+
+var b = [[{ text: "Deebi'ii", callback_data: "/myor"}]]
+
+Api.sendDocument({
+  chat_id: chat.chatid,
+  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/2316)",reply_markup: { inline_keyboard: b },on_result: "/msgid"})
+
+Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
