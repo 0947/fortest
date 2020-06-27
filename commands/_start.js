a226f366b0cf6fb14a334a869a20c28791bfb76e
Bots.Business <git.sync@bots.business>
2020-06-27 09:02:34 +0000

    Bots.Business export for bot id: 87893

--- commands/_start.js
+++ commands/_start.js
@@ -9,17 +9,23 @@
   aliases: 
 CMD*/
 
-let msg = User.getProperty("msgid");
-var buttons = [
-[ {title: "📢 ወደ ቻነሉ 📢", url: "http://t.me/catholicb" } ],
-[ {title: "✅ ተቀላቅያለሁ", command: "/mytg" } ]
-]
-Bot.sendInlineKeyboard(buttons, "*🤖ይህንን ቦት ለመጠቀም በቅድሚያ ከታች ያለውን ቻነል ይቀላቀሉ\n\n❗️ይህንን ቦት ለመጠቀም የ* @catholicb *አባል መሆን ያስፈልጋል፡፡\n\n✔️ከዚህ በፊት ተቀላቅለው ከሆነ ከታች ☑️ተቀላቅያለሁ የሚለውን ይጫኑ፡፡\n\n⚠️ቻነሉን ካልተቀላቀሉ ቦቱ አይሰራም፡፡\n\n🤖Hello dear*  "+ user.first_name +"\n*to use this service Join the channel below*",{on_result:"/msgid"})
-
-Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
-
 Api.deleteMessage({chat_id:chat.chatid,message_id:request.message_id})
 
 Api.sendChatAction({
 chat_id:chat.chatid,
 action:"typing"})
+
+if(!params){
+Bot.run({command: "/start aa", run_after:2 })
+}else{
+let msg = User.getProperty("msgid");
+var buttons = [
+[ {title: "📣 ወደ ቻነሉ 📣", url: "http://t.me/catholicb" } ],
+[ {title: "✅ ተቀላቅያለሁ", command: "/mytg" } ]
+]
+Bot.sendInlineKeyboard(buttons, "*🤖ይህንን ቦት ለመጠቀም በቅድሚያ ከታች ያለውን ቻነል ይቀላቀሉ\n\n❗️ይህንን ቦት ለመጠቀም የ* @catholicb *አባል መሆን ያስፈልጋል፡፡\n\n✔️ከዚህ በፊት ተቀላቅለው ከሆነ ከታች ☑️ተቀላቅያለሁ የሚለውን ይጫኑ፡፡\n\n⚠️ቻነሉን ካልተቀላቀሉ ቦቱ አይሰራም፡፡\n\n🤖Hello dear*  "+ user.first_name +"\n*to use this service Join the channel below*",{on_result:"/msgid"})
+
+Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
+}
+
+
