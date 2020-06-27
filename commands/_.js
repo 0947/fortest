c2faf12652fb2c9bbaf04194229d3429ade8ccff
Bots.Business <git.sync@bots.business>
2020-06-27 06:33:23 +0000

    Bots.Business export for bot id: 87893

--- commands/_.js
+++ commands/_.js
@@ -12,11 +12,6 @@
   aliases: 
 CMD*/
 
-Bot.sendInlineKeyboard(
-  [
-  { title: "✅ዋና ገጽ", command: "/start"}], 
-"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🚨"+message+"\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})
-
 let msg=User.getProperty("msgid")
 
 Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
@@ -30,22 +25,46 @@ let au = request.audio
 let pd = request.document
 let vi = request.video
 let vo = request.voice
+let st = request.sticker
 
 if(ms){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n💬 "+message+"\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
 if(au){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🎙Audio File\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
 if(vi){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🎥Video File\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
 if(p){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n📸Photo\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
 if(pd){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🎞File\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
 if(vo){
 Bot.runCommand("/dsa")
-}
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🗣Voice Message\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
+if(st){
+Bot.sendInlineKeyboard(
+  [
+  { title: "✅ዋና ገጽ", command: "/start"}], 
+"*😊ውድ*  "+ user.first_name+"\n*❗️ወደ ሰርቨራችን ያስገቡት መረጃ*\n\n🎭Sticker\n\n⚠️*እባክዎትን በተኖችን ብቻ ይጠቀሙ፡፡ ወደ ዋና ገጽ ለመሄድ ከታች ያለውን በተን ይጠቀሙ*\n\n"+user.first_name+"  🚫Unknown Command inserted please use inline buttons only",{on_result:"/msgid"})}
