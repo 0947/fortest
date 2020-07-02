af63730e126b0e56ef9d58d8e33c9f17085f8bea
Bots.Business <git.sync@bots.business>
2020-07-02 06:45:10 +0000

    Bots.Business export for bot id: 87893

---
+++ commands/covid - 19/_coval.js
@@ -0,0 +1,25 @@
+/*CMD
+  command: /coval
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: covid - 19
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+let msg = User.getProperty("msgid");
+
+var b =[
+[{title:"A",command:"/ca"},{title:"B",command:"/cb"},{title:"C",command:"/cc"},
+{title:"D",command:"/cd"},{title:"E",command:"/ce"}],[{title:"F",command:"/cf"},
+{title:"G",command:"/cg"},{title:"H",command:"/ch"},{title:"I",command:"/ci"},
+{title:"J",command:"/cj"}],[{title:"K",command:"/ck"},{title:"L",command:"/cl"},
+{title:"M",command:"/cm"},{title:"N",command:"/cn"},{title:"O",command:"/co"}],
+[{title:"P",command:"/cp"},{title:"Q",command:"/cq"},{title:"R",command:"/cr"},
+{title:"S",command:"/cs"},{title:"T",command:"/ct"}],[{title:"U",command:"/cu"},
+{title:"V",command:"/cv"},{title:"W",command:"/cw"},{title:"Y",command:"/cy"},{title:"Z",command:"/cz"}],
+[{title:"✅Go Back / ተመለስ",command:"/mycov"}]]
+
+Bot.editMessage(user.first_name+" *🇺🇸Please Select The First ALPHABET of the country name.\n\n🇪🇹እባክዎትን የሀገሩን ስም የመጀመሪያ ሆሄ ይምረጡ፡፡*",msg)&Bot.editInlineKeyboard(b,msg)
