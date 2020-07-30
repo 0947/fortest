4c8642357809b27873f37e7045d4f0ae10ec6b59
Sam <64862632+0947@users.noreply.github.com>
2020-07-30 15:24:46 +0300

    Update _menu.js

--- commands/_menu.js
+++ commands/_menu.js
@@ -14,5 +14,4 @@ let msg = User.getProperty("msgid");
 var b=[
 [{ title: "❗️ይህንን ይጫኑ", command: "/mych"},{title:"❗️Home Page",command:"/mych"}]]
 
-Bot.editMessage("*ውድ  "+ user.first_name+" 
- 💯ወደ ዋና ገጽ ለመሄድ ከታች ያለውን ይጫኑት✅*",msg)&Bot.editInlineKeyboard(b,msg)
+Bot.editMessage("*ውድ  "+ user.first_name+"   💯ወደ ዋና ገጽ ለመሄድ ከታች ያለውን ይጫኑት✅*",msg)&Bot.editInlineKeyboard(b,msg)
