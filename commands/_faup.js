bd714a400408c11ba3114a70cc570c9f76d95b63
Sam <64862632+0947@users.noreply.github.com>
2020-09-07 08:11:56 +0300

    Update _faup.js

--- commands/_faup.js
+++ commands/_faup.js
@@ -18,12 +18,3 @@ var b = [[{text:"⬅️ተመለስ",callback_data:"/mych"}]]
   document: "https://t.me/xexgbjnsjnjudxbjjdxSam/3376",caption:  "✅የቅድስት ፋውስቲና ማስታወሻ በአማርኛ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
 
 Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
-let msg=User.getProperty("msgid")
-
-var b = [[{text:"⬅️ተመለስ",callback_data:"/mych"}]]
-
- Api.sendDocument({
-  chat_id: chat.chatid,
-  document: "https://t.me/xexgbjnsjnjudxbjjdxSam/3376",caption:  "✅የቅድስት ፋውስቲና ማስታወሻ በአማርኛ", reply_markup: { inline_keyboard: b },on_result: "/msgid"})
-
-Api.deleteMessage({chat_id:chat.chatid,message_id:msg})
